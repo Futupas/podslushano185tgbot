bb3d6a241e901188bd951a02dac3f355f4c7d23d
Olexandr Panov <futupas@gmail.com>
2018-09-28 21:55:03 +0300

    use strtolower_my

--- bot.php
+++ bot.php
@@ -1,4 +1,15 @@
 <?php
+    function strtolower_my($str) {
+        $str2 = strtolower($str);
+        $search = array(
+        'Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я','Ч','С','М','И','Т','Ь','Б','Ю','Ё','І','Ї'
+        );
+        $replace = array(
+        'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э','я','ч','с','м','и','т','ь','б','ю','ё','і','ї'
+        );
+        $str2 = str_replace($search, $replace, $str2);
+        return $str2;
+    };
     function SendMessage($chatid, $text) {
         $response = file_get_contents('https://api.telegram.org/bot'.getenv('bot_token').'/sendMessage?chat_id='.$chatid.'&text='.urlencode($text));
     };
@@ -15,7 +26,7 @@
         };
 
         $msg = $requestData->message->text;
-        $lowermsg = strtolower($msg);
+        $lowermsg = strtolower_my($msg);
 
         if ($lowermsg == '/start') {
             SendMessage($msg_chatid, '');
